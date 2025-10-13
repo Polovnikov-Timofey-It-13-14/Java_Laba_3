87552ccb4572491363a6dab8699291fb74108238
Polovnikov-Timofey-It-13-14 <polovnikovtu999@gmail.com>
2025-10-13 08:27:04 +0500

    Update Calculate.java

--- Laba3/src/ru/Polovnikov/Main/Calculate.java
+++ Laba3/src/ru/Polovnikov/Main/Calculate.java
@@ -3,3 +3,13 @@ package ru.Polovnikov.Main;
 public interface Calculate {
     double decimal();
 }
+
+    //Сумма через интерфейс
+    public static double sumNumbers(Calculate... values) {
+        double sum = 0;
+        for (Calculate value : values) {
+            sum += value.decimal();
+        }
+        return sum;
+    }
+
